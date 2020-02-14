94410b6f34eb895578af5b98adca5b8e0dec8f01
monirozzamanroni <monirozzaman35-223@diu.edu.bd>
2020-02-15 00:57:47 +0600

    Change Package Name

---
+++ src/main/java/com/itvillage/jwtauthentication/dto/request/LoginForm.java
@@ -0,0 +1,20 @@
+package com.itvillage.jwtauthentication.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Setter
+@Getter
+public class LoginForm {
+    @NotBlank
+    @Size(min=3, max = 60)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+}
---
+++ src/main/java/com/itvillage/jwtauthentication/dto/request/SignUpForm.java
@@ -0,0 +1,24 @@
+package com.itvillage.jwtauthentication.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Setter
+@Getter
+public class SignUpForm {
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+}

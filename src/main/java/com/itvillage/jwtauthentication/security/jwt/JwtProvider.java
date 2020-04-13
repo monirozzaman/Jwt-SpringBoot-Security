437080b05776dd982bd348880c596b62a6fffcfc
monirozzamanroni <monirozzaman35-223@diu.edu.bd>
2020-04-13 18:25:40 +0600

    Add Climes

--- src/main/java/com/itvillage/jwtauthentication/security/jwt/JwtProvider.java
+++ src/main/java/com/itvillage/jwtauthentication/security/jwt/JwtProvider.java
@@ -6,9 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtProvider {
@@ -22,11 +26,15 @@ public class JwtProvider {
     private int jwtExpiration;
 
     public String generateJwtToken(Authentication authentication) {
-
+        List<GrantedAuthority> authorities = new ArrayList<>(authentication.getAuthorities());
         UserPrinciple userPrincipal = (UserPrinciple) authentication.getPrincipal();
 
+        Claims claims = Jwts.claims();
+        claims.setSubject(userPrincipal.getUsername());
+        claims.put("scopes", authorities.stream().map(s -> s.toString()).collect(Collectors.toList()));
+
         return Jwts.builder()
-		                .setSubject((userPrincipal.getUsername()))
+                .setClaims(claims)
 		                .setIssuedAt(new Date())
 		                .setExpiration(new Date((new Date()).getTime() + jwtExpiration*1000))
 		                .signWith(SignatureAlgorithm.HS512, jwtSecret)
